65661b4ee194a232fc6da580beeb21452928d3ff
Evgenija-P <99011054+Evgenija-P@users.noreply.github.com>
2022-11-10 12:56:54 +0200

    filter

--- src/components/ContactList/ContactList.js
+++ src/components/ContactList/ContactList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { Loader, Placeholder } from 'rsuite';
 import { fetchContacts } from 'redux/operations';
 import { List, Item } from './ContactList.styled';
 import { Contact } from 'components/Contact/Contact';
@@ -16,13 +17,18 @@ export const ContactList = () => {
   }, [dispatch]);
 
   const normalized = filter.toLocaleLowerCase();
-  const contactsState = contacts.filter(contact =>
+  const contactsState = contacts?.filter(contact =>
     contact.name.toLocaleLowerCase().includes(normalized)
   );
 
   return (
     <List>
-      {isLoading && <p>Loading tasks...</p>}
+      {isLoading && (
+        <div>
+          <Placeholder.Paragraph rows={8} />
+          <Loader center content="loading" />
+        </div>
+      )}
       {error && <p>{error}</p>}
       {contacts &&
         contactsState.map(({ id, name, phone, avatar, mail }) => (
